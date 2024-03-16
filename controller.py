c9df1a2a21224983e0fd43ebd246186f3984660e
IDGS-803-21000448 <carene2803@gmail.com>
2024-03-16 08:16:56 -0600

    add: Estructura Controlador

---
+++ controller.py
@@ -0,0 +1,23 @@
+from models import *
+
+
+def obtenerPedidos():
+    return True
+
+def obtenerMateriaPrima():
+    return True
+
+def verificarPermisos(rol, modulo, accion):
+    return True
+
+
+def verificarCaducidades():
+    obtenerMateriaPrima()
+    return True
+
+def solicitarProduccion():
+    return True
+
+
+def producir():
+    return True
