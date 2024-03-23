7a141f46b8966908704e7bf03244f4598911b2e8
IDGS-803-21000008 <davidfigueroa0777@gmail.com>
2024-03-23 11:02:11 -0600

    se agrego el metodo de buscar en usuarios

--- controllers/controller_usuarios.py
+++ controllers/controller_usuarios.py
@@ -27,23 +27,36 @@ def agregarUsuario(form):
 
 
 def modificarUsuario(form, id):
+    contrasenaAModificar = form.contrasena.data
+
+    if contrasenaAModificar == '' or contrasenaAModificar is None:
+        # Si la contraseña está vacía en el formulario, obtén la contraseña actual del usuario desde la base de datos
+        try:
+            contrasena_actual = db.session.execute(
+                text("SELECT contrasena FROM user WHERE id = :id"),
+                {'id': id}
+            ).fetchone()[0]  # Obtén la primera columna de la primera fila
+            contrasenaAModificar = contrasena_actual
+        except Exception as e:
+            print(f"Error al obtener la contraseña del usuario con ID {id}:", e)
+            return False  # Indica que la operación falló
+
     try:
         db.session.execute(
-        text("CALL modificar_usuario(:id, :nombre, :puesto, :rol, :estatus, :usuario, :contrasena)"),
-        {
-            'id': id,
-            'nombre': form.nombre.data,
-            'puesto': form.puesto.data,
-            'rol': form.rol.data,
-            'estatus': 'Activo',
-            'usuario': form.usuario.data,
-            'contrasena': form.contrasena.data,
-        })
+            text("CALL modificar_usuario(:p_id, :p_nombre, :p_puesto, :p_rol, :p_estatus, :p_usuario, :p_contrasena)"),
+            {
+                'p_id': id,
+                'p_nombre': form.nombre.data,
+                'p_puesto': form.puesto.data,
+                'p_rol': form.rol.data,
+                'p_estatus': 'Activo',
+                'p_usuario': form.usuario.data,
+                'p_contrasena': contrasenaAModificar,
+            })
         db.session.commit()
         return True  # Indica que la operación se realizó correctamente
     except Exception as e:
         db.session.rollback()  # Deshace los cambios en caso de error
-        # Maneja el error de alguna manera (por ejemplo, registrándolo o lanzándolo nuevamente)
         print(f"Error al modificar usuario con ID {id}:", e)
         return False  # Indica que la operación falló
 
--- formularios/formUsuario.py
+++ formularios/formUsuario.py
@@ -22,11 +22,39 @@ class UsersForm(Form):
         validators.DataRequired(message='El campo es requerido'),
         validators.length(min=4, max=15, message='Ingresa un usuario valido')
     ])
-    contrasena = StringField('Contraseña', [
+    contrasena = PasswordField('Contraseña', [
         validators.DataRequired(message='El campo es requerido'),
         validators.length(min=4, max=15, message='Ingresa una contraseña valida')
     ])
-    confirmar_contrasena = StringField('Confirmar contraseña', [
+    confirmar_contrasena = PasswordField('Confirmar contraseña', [
         validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=15, message='Ambas contraseñas deben de coincidir.')
+    ])
+    
+class UsersFormModificar(Form):
+    id = IntegerField('')
+    nombre=StringField('Nombre', [
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=15, message='Ingresa un nombre valido')
+    ])
+    puesto=StringField('Puesto', [
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=15, message='Ingresa un apellido paterno valido')
+    ])
+    rol = SelectField('Rol', choices=[
+        ('admin', 'Administrador'), ('venta', 'Ventas'), ('produccion', 'Producción'), ('inventario', 'Inventario')
+        ], validators=[validators.DataRequired(message='El campo es requerido.')])
+    # estatus = SelectField('Estatus', choices=[('activo', 'Activo'), ('inactivo', 'Inactivo')], validators=[validators.DataRequired(message='El campo es requerido.')])
+
+    usuario = StringField('Usuario', [
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=15, message='Ingresa un usuario valido')
+    ])
+    contrasena = PasswordField('Contraseña', [
+       # validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=15, message='Ingresa una contraseña valida')
+    ])
+    confirmar_contrasena = PasswordField('Confirmar contraseña', [
+      #  validators.DataRequired(message='El campo es requerido'),
         validators.length(min=4, max=15, message='Ambas contraseñas deben de coincidir.')
     ])
