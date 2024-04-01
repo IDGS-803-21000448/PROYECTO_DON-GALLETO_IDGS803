8493d3aa702a1ac001e63283d9aab4abe8bc4a95
IDGS-803-21000448 <carene2803@gmail.com>
2024-03-31 23:11:07 -0600

    Campana de alertas dinamicas

--- modules/index/routes.py
+++ modules/index/routes.py
@@ -3,6 +3,7 @@ from flask_login import login_required
 from controllers.controller_login import requiere_rol, requiere_token
 from flask_login import current_user
 from controllers import controller_mermas
+from models import Alerta
 from . import index
 
 @index.route('/index', methods=["GET"])
@@ -11,4 +12,5 @@ from . import index
 def index():
     print(f"USUARIO: {current_user}")
     controller_mermas.verificarCaducidades()
-    return render_template("index.html")
+    alertas = Alerta.query.filter_by(estatus = 0).all()
+    return render_template("index.html", countAlertas = len(alertas))
