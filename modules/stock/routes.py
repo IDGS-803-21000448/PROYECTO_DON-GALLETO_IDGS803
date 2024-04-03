ce2b0556f308e77f250e91102d9f9044fdc68ab5
IDGS-803-21000029 <21000029@alumnos.utleon.edu.mx>
2024-04-03 10:50:04 -0600

    Correccion de Encriptado y Calculo de precio por paquetes

--- modules/stock/routes.py
+++ modules/stock/routes.py
@@ -4,6 +4,7 @@ from controllers.controller_login import requiere_rol
 from flask_login import login_required
 from controllers.controller_login import requiere_token
 from models import db, CostoGalleta, Receta
+import math
 
 
 @stock.route('/moduloStock')
@@ -18,9 +19,26 @@ def modulo_stock():
     for nombre_galleta, id_precio in recetas_con_precio:
         costo_galleta = CostoGalleta.query.filter_by(id=id_precio).first()
         if costo_galleta:
+            # obtiene la cantidad de galletas que hay en 700 gramos
+            cantidad_700_gramos = math.ceil(700 / 30.0)
+            print(cantidad_700_gramos)
+
+            # obtiene la cantidad de galletas que hay en 1000 gramos
+            cantidad_1000_gramos = math.ceil(1000 / 30.0)
+            print(cantidad_1000_gramos)
+
+            # calcula el precio por cantidad de galletas en 700 gramos con ganancia de 10%
+            precio_700_gramos = math.ceil((costo_galleta.precio * cantidad_700_gramos) * 1.1)
+
+            # calcula el precio por cantidad de galletas en 1000 gramos con ganancia de 15%
+            precio_1000_gramos = math.ceil((costo_galleta.precio * cantidad_1000_gramos) * 1.15)
+
             galletas.append({
                 'nombre': nombre_galleta,
-                'cantidad_disponible': costo_galleta.galletas_disponibles
+                'cantidad_disponible': costo_galleta.galletas_disponibles,
+                'precio_unitario': costo_galleta.precio, 
+                'precio_pkt_700': precio_700_gramos,
+                'precio_pkt_1000': precio_1000_gramos
             })
 
     return render_template('moduloStock/vistaStock.html', galletas=galletas)
